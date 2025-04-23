aca445fb9c3cf3847a3c540c342b80b279e0d737
y.dang <you@example.com>
2025-04-23 10:08:32 +0900

    Java課題0422

---
+++ src/jp/co/aforce/sample/DoubleInteger.java
@@ -0,0 +1,11 @@
+package jp.co.aforce.sample;
+
+public class DoubleInteger {
+		 public static void main(String args[]) {
+			 int a=5;
+			 int b=2;
+			 int c=a*b;
+			 System.out.println(a+"×"+b+"="+c);			 
+		 }
+
+}
