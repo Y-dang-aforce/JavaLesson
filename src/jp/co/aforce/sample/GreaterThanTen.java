0fc1021e9954cb8ce2e18443da30d7dcb53887c1
y.dang <you@example.com>
2025-04-23 10:10:16 +0900

    Java課題0422

--- src/jp/co/aforce/sample/GreaterThanTen.java
+++ src/jp/co/aforce/sample/GreaterThanTen.java
@@ -1,5 +1,13 @@
 package jp.co.aforce.sample;
 
 public class GreaterThanTen {
-
+	public static void main(String args[]) {
+		int num=12;
+		
+		if(num>10) {
+			System.out.println("numは10より大きいです。");
+			
+			
+		}
+	}
 }
